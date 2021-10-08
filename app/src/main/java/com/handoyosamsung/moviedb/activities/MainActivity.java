c92ea1826b68d08b53498aa51fc9238d754a36d1
Itsuka22 <111201911829@mhs.dinus.ac.id>
2021-10-08 15:39:58 +0700

    first

---
+++ app/src/main/java/com/handoyosamsung/moviedb/activities/MainActivity.java
@@ -0,0 +1,100 @@
+package com.handoyosamsung.moviedb.activities;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+
+import com.handoyosamsung.moviedb.fragment.FragmentFavorite;
+import com.handoyosamsung.moviedb.fragment.FragmentMovie;
+import com.handoyosamsung.moviedb.R;
+import com.handoyosamsung.moviedb.utils.BottomBarBehavior;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+//
+
+        BubbleNavigationLinearView navigationBar = findViewById(R.id.navigationBar);
+
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigationBar.getLayoutParams();
+        layoutParams.setBehavior(new BottomBarBehavior());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FragmentMovie()).commit();
+
+        navigationBar.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                switch (position) {
+                    case 0:
+                        fragment = new FragmentMovie();
+                        break;
+                    case 1:
+                        fragment = new FragmentFavorite();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+            }
+        });
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        window.setAttributes(winParams);
+    }
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this) ;
+        builder.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+        builder.setTitle("Setuju Keluar");
+        builder.setMessage("Apakah Kamu ingin Keluar?");
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Batal",null);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/handoyosamsung/moviedb/networking/ApiEndpoint.java
@@ -0,0 +1,18 @@
+package com.handoyosamsung.moviedb.networking;
+
+
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "http://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=0dde3e9896a8c299d142e214fcb636f8";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+    public static String URLFILM = "https://www.themoviedb.org/movie/";
+
+
+}
